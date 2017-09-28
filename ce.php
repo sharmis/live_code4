7f97bd1a4d50353f47c4c7ab9e785f0d050e54da
MilesZhao <mileszhao1990@yahoo.com>
2017-09-28 14:03:12 -0400

    logica and array operators

--- ce.php
+++ ce.php
@@ -136,7 +136,118 @@ echo "<br>";
 echo ++$c - --$d;
 echo "<br>";
 
+echo "<h2>Logical Operators</h2>";
+$x = 100;  
+$y = 50;
+if ($x == 100 and $y == 50) {
+    echo "Hello world!";
+}
+echo "<br>";
+$x = 100;  
+$y = 50;
+if ($x == 100 or $y == 80) {
+    echo "Hello world!";
+}
+echo "<br>";
+$x = 100;  
+$y = 50;
+if ($x == 100 xor $y == 80) {
+    echo "Hello world!";
+}
+echo "<br>";
 
+$x = 100;  
+$y = 50;
+if ($x == 100 && $y == 50) {
+    echo "Hello world!";
+}
+echo "<br>";
+$x = 100;  
+$y = 50;
+if ($x == 100 || $y == 80) {
+    echo "Hello world!";
+}
+echo "<br>";
+
+$x = 100;  
+if (! ($x != 90) ) {
+    echo "Hello world NOt";
+}
+echo "<br>";
+
+echo "<h2>Array Operators</h2>";
+
+$arr = array('I','Love','Web','Application');
+echo implode(" ",$arr)."<br>";
+echo implode("+",$arr)."<br>";
+echo implode("-",$arr)."<br>"; 
+echo implode("*",$arr)."<br>";
+
+echo "<br>";
+$str = "I Love Web Application";
+//zero limit
+print_r(explode(" ",$str,0));
+echo "<br>";
+//positive limit
+print_r(explode(" ",$str,2));
+echo "<br>";
+//negative limit
+print_r(explode(" ",$str,-1));
+echo "<br>";
+echo "<br>";
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+print_r($x + $y); 
+echo "<br>";
+echo "<br>";
+$num1 = range(0,3);
+$num2 = range(2,7);
+print_r($num1); 
+echo "<br>";
+print_r($num2); 
+echo "<br>";
+print_r($num1 + $num2); 
+echo "<br>";
+
+$first = array('one','c'=>'three');
+$second = array('a'=>'fourth','c'=>'sixth','3'=>'number');
+
+echo "<br>";
+print_r($first);
+echo "<br>";
+print_r($second);
+echo "<br>";
+print_r($first + $second);
+echo "<br>";
+
+echo "<br>";
+$x = array("a" => "red", "c" => "blue");  
+$y = array("c" => "blue", "a" => "red");  
+var_dump($x == $y);
+echo "<br>";
+
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+var_dump($x != $y);
+echo "<br>";
+var_dump($x <> $y);
+echo "<br>";
+
+echo "<br>";
+$x = array("a" => "1", "b" => "2");  
+$y = array("a" => 1, "b" => 2);  
+var_dump($x == $y);
+echo "<br>";
+var_dump($x === $y);
+echo "<br>";
+
+echo "<br>";
+$x = array("a" => "1", "b" => '2');  
+$y = array("b" => "2", "a" => '1'); 
+var_dump($x == $y);
+echo "<br>"; 
+var_dump($x === $y);
+echo "<br>";
 
 echo "<br><br><br><br><br><br><br><br>";
 echo "<br><br><br><br><br><br><br><br>";
